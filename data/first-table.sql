521a4b0a22fe9b26b42838932315ff55a31bd683
Jonathan Salguero Gómez <jonathansalguero91@gmail.com>
2020-11-17 23:27:39 -0600

    table schema, queries, bonus and plots upload

---
+++ data/first-table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE employees(
+	emp_no INT PRIMARY KEY,
+	birth_date VARCHAR(30),
+	first_name VARCHAR(30),
+	last_name VARCHAR(30),
+	gender VARCHAR(30),
+	hire_date DATE
+);
+
+SELECT * FROM employees
